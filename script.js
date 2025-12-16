a09c45112d067b79dd189cb95a0c149f89f9315f
Leo-Raud <LEO.RAUD@latoile.em-lyon.com>
2025-12-16 11:13:28 +0100

    avancee1

---
+++ script.js
@@ -0,0 +1,25 @@
+//  Etape 1 : mettre en place le ftech pour lié le Json
+
+//  Etape 2 :
+//  Pour le header :
+//      -mettre tout d'abord le nom de l'entreprise avec "data.nomCommercial"
+//      -Faire la nav (sur html où chaque boutton amène à une partie de la page)
+//      -Faire un boutton appeller qui sert à rien
+
+//  Etape 3 :
+//  Pour la hero-section :
+//      -Faire un H1 qui montre le "data.phraseAccroche"
+//      -faire un Paragraphe qui montre le "texteAppelAction"
+//      -Faire deux bouttons l'un pour découvrir (amène à la liste des salles)
+//       et l'autre qui est pour payer mais sert à rien
+
+//  Etape 4 :
+//      -faire avec le html un titre de section qui servira pour les avantages client
+//      -faire un foreach sur "avantagesClients" pour crée des cartes sur chaque avantages
+
+// Etape 5 :
+//      -faire une section où l'on a chaque salle dans une carte
+//      -La carte de salle doit faire apparaitre :
+//          -une image de la salle avec "data.activities.image-url"
+//          -une image de la salle avec "data.activities.nom"
+//          -une image de la salle avec "data.activities.description"
